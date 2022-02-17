9c3aaaf413237f88e71724ba7107c2575418d438
张旭 <865381573@qq.com>
2022-02-17 09:31:54 +0800

    feat:修改

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">表单生成器</router-link> |
-      <router-link to="/about">关于</router-link>
+      <!-- <router-link to="/about">关于</router-link> -->
     </div>
     <transition name="component-fade" mode="out-in">
       <router-view class="body-view" />
